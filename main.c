07f18255ff186d848485c4d2cc284f702e37a803
Yash patil <149749700+PATILYASHH@users.noreply.github.com>
2024-02-26 14:19:05 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int  main()
+{
+  int a , b , r  ;
+  
+  restart:
+  
+  printf("Enter value of a : ");
+scanf("%d",&a);
+  
+  printf("Enter value of b : ");
+ scanf("%d",&b);
+
+int c = b;
+  int d = a;
+  
+if(a >= b)
+{
+  
+  while(c != 0)
+    {
+      r  =  d % c;
+      d = c ;
+      c = r;
+    }
+  printf("GCD of %d and %d is %d ",a,b,d);
+}
+else
+{
+  printf("Pleese make a greater than or equals to b ");
+printf("\n\n");
+  goto restart;
+}
+
+  int lcm = (a*b)/d;
+
+  printf("\nLCM of %d and %d is %d",a,b,lcm);
+  return 0;
+}
+
